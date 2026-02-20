a004e41793254e42e09694434d82670327b093ea
SimonS <simonsenkyrik@email.cz>
2026-02-20 11:04:15 +0100

    React + Tailwind + ShadCN

---
+++ cloudtech-dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl text-red-500">Test</h1>
+      <Button >Test</Button>
+    </>
+  )
+}
+
+export default App
